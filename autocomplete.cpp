807abfa86e18032515ff0d1a95c6e3249bcd92dc
yaseen <yaseenmohommad7@gmail.com>
2024-10-19 16:48:10 +0530

    formatting changes

--- autocomplete.cpp
+++ autocomplete.cpp
@@ -1,74 +1,74 @@
 #include "main.h"
 
 bool sanitize(string str){
-  istringstream tok(str);
-  vector<string> res;
-  string tmp;
-  while(tok >> tmp){
-    res.push_back(tmp);
-  }
-  return res.empty();
+    istringstream tok(str);
+    vector<string> res;
+    string tmp;
+    while(tok >> tmp){
+        res.push_back(tmp);
+    }
+    return res.empty();
 }
 
 pair<string,bool> autocomplete(string str){
-  vector<string> comp;
-  string r1;
-  bool flag = 0;
-  if(str.empty()){str = {};}
-  //cout << str << "this  is the string " << "\n";
-  vector<string> res;
-  struct dirent* dir_val;
-  DIR * dir = opendir(".");
-  while((dir_val = readdir(dir)) != NULL){
-    res.push_back(dir_val->d_name);
-  }
+    vector<string> comp;
+    string r1;
+    bool flag = 0;
+    if(str.empty()){str = {};}
+    //cout << str << "this  is the string " << "\n";
+    vector<string> res;
+    struct dirent* dir_val;
+    DIR * dir = opendir(".");
+    while((dir_val = readdir(dir)) != NULL){
+        res.push_back(dir_val->d_name);
+    }
 
-  if(!sanitize(str)){
-    vector<string> in;
-    string tmp;
-    istringstream tok(str);
-    while(tok >> tmp){
-      in.push_back(tmp);
+    if(!sanitize(str)){
+        vector<string> in;
+        string tmp;
+        istringstream tok(str);
+        while(tok >> tmp){
+            in.push_back(tmp);
+        }
+
+        for(auto i : res){
+            if(i.find(in[in.size() - 1]) != string::npos && i[0] == (in[in.size()-1])[0]){
+                comp.push_back(i);
+            }
+        }
+        string part_st{};
+        if(comp.size() == 0){
+            r1 = {};
+        }
+        else if(comp.size() ==1){
+            if(comp[0] == in[in.size()-1]){r1 = str;}
+            else{
+                int l = str.find(in[in.size() -1]);
+                int e = l + (in[in.size() -1]).length();
+                str.erase(str.begin()+l,str.begin()+e);
+                r1 = str + comp[0];
+                flag = 1;
+            }
+        }
+        else if(comp.size() > 1){
+            part_st = comp[0];
+            for(int i = 1; i < comp.size(); ++i){
+                part_st = part_st + "\t" +comp[i];
+            }
+            r1 = part_st;
+        }
     }
-  
-    for(auto i : res){
-      if(i.find(in[in.size() - 1]) != string::npos && i[0] == (in[in.size()-1])[0]){
-	comp.push_back(i);
-      }
+    else if(res.size() > 2){
+        // r1 = res[2];
+        for(int i = 2; i < res.size(); ++i){
+            r1 = r1 +"\n"+ res[i];
+        }
+        r1 = r1 + "\n";
     }
-    string part_st{};
-    if(comp.size() == 0){
-      r1 = {};
+    else{
+        r1 = string{};
     }
-    else if(comp.size() ==1){
-      if(comp[0] == in[in.size()-1]){r1 = str;}
-      else{
-	int l = str.find(in[in.size() -1]);
-	int e = l + (in[in.size() -1]).length();
-	str.erase(str.begin()+l,str.begin()+e);
-	r1 = str + comp[0];
-	flag = 1;
-      }
-    }
-    else if(comp.size() > 1){
-      part_st = comp[0];
-      for(int i = 1; i < comp.size(); ++i){
-	part_st = part_st + "\t" +comp[i];
-      }
-      r1 = part_st;
-    }
-  }
-  else if(res.size() > 2){
-    // r1 = res[2];
-    for(int i = 2; i < res.size(); ++i){
-      r1 = r1 +"\n"+ res[i];
-    }
-    r1 = r1 + "\n";
-  }
-  else{
-    r1 = string{};
-  }
-  closedir(dir);
-  pair<string,bool> ret = make_pair(r1,flag);
-  return ret;
+    closedir(dir);
+    pair<string,bool> ret = make_pair(r1,flag);
+    return ret;
 }
--- pinfo.cpp
+++ pinfo.cpp
@@ -2,37 +2,37 @@
 
 extern int global_parent_pid;
 string pinfo(int val){
-  string res{};
-  int pid = val;
-  char* buff =  new char[1000];
-  char* exe_buff = new char[120];
-  if(val == 0){
-    pid = getpid();
-  }
-  const string  path_name = "/proc/"+to_string(pid) + "/stat";
-  const string  exe_path = "/proc/"+to_string(pid) + "/cmdline";
-  int fd = open(path_name.c_str(),O_RDONLY);
-  int wd = open(exe_path.c_str(),O_RDONLY);
-  read(fd,buff,1000);
-  read(wd,exe_buff,100);
-  vector<string> l;
-  char *ptr;
-  ptr = strtok(buff," ");
-  while(ptr != NULL){
-    l.push_back(string(ptr));
-    ptr = strtok(NULL," ");
-  }
-  string state = l[2];
-  if(getpgid(pid) == getpgid(global_parent_pid)){
-    state = state + "+";
-  }
-  string exe = string(exe_buff);
-  long long mb = 1024*1024;
-  long long  vmsize = stol(l[22]);
-  res = "pid: "+ l[0]+"\n"  +"ProcessStatus: "+ state + "\n" +"executable path: "+ exe + "\n" +"virutal memory: "+ to_string(vmsize) +"\n" ;
-  close(fd);
-  close(wd);
-  delete[] exe_buff;
-  delete[] buff;
-  return res;
+    string res{};
+    int pid = val;
+    char* buff =  new char[1000];
+    char* exe_buff = new char[120];
+    if(val == 0){
+        pid = getpid();
+    }
+    const string  path_name = "/proc/"+to_string(pid) + "/stat";
+    const string  exe_path = "/proc/"+to_string(pid) + "/cmdline";
+    int fd = open(path_name.c_str(),O_RDONLY);
+    int wd = open(exe_path.c_str(),O_RDONLY);
+    read(fd,buff,1000);
+    read(wd,exe_buff,100);
+    vector<string> l;
+    char *ptr;
+    ptr = strtok(buff," ");
+    while(ptr != NULL){
+        l.push_back(string(ptr));
+        ptr = strtok(NULL," ");
+    }
+    string state = l[2];
+    if(getpgid(pid) == getpgid(global_parent_pid)){
+        state = state + "+";
+    }
+    string exe = string(exe_buff);
+    long long mb = 1024*1024;
+    long long  vmsize = stol(l[22]);
+    res = "pid: "+ l[0]+"\n"  +"ProcessStatus: "+ state + "\n" +"executable path: "+ exe + "\n" +"virutal memory: "+ to_string(vmsize) +"\n" ;
+    close(fd);
+    close(wd);
+    delete[] exe_buff;
+    delete[] buff;
+    return res;
 }
